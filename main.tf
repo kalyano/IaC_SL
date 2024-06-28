340efef6262517fbfd90fe41e33a85b7f5751c09
Kal <58021911+kalyano@users.noreply.github.com>
2024-06-28 17:00:33 -0400

    Update and rename simple.tf to main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,6 @@ resource "aws_instance" "web" {
     ami = "ami-0574da719dca65348"
     instance_type = "t2.micro"
     tags = {
-        Name = "web-server-1"
+        Name = "web-server-testEnv"
     }
 }
