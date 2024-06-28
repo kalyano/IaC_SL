9bbe3200795664e10b9f76d206b7483ed4601b42
Kal <kalyanoduri1@gmail.com>
2024-06-27 20:29:32 -0400

    simple-tf

---
+++ simple.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.16"
+        }
+    }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    region = "us-east-1"
+}
+
+# ec2 instance with web server
+resource "aws_instance" "web" {
+    ami = "ami-0574da719dca65348"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "web-server-1"
+    }
+}
